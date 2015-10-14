f3275baac528bc6608aed50d1153bc32e95352cb
msukenik <m45sukenik@gmail.com>
2015-10-14 16:28:53 +0200

    rain

--- Assets/Scripts/Game/TopCarColliderScript.cs
+++ Assets/Scripts/Game/TopCarColliderScript.cs
@@ -20,4 +20,41 @@ public class TopCarColliderScript : MonoBehaviour {
 
 			
 	}
+
+	/*void OnTriggerEnter2D(Collider2D coll) {
+		if(coll.GetComponent<Collider2D>().CompareTag("Coin")) {
+			GameObject coin = coll.GetComponent<Collider2D>().gameObject;
+			AudioSource.PlayClipAtPoint(pickupCoinClips, transform.position);
+			coin.SendMessage ("CoinReact");
+		}
+		
+		if(coll.GetComponent<Collider2D>().CompareTag("InkBottle")) {
+			GameObject inkBottle = coll.GetComponent<Collider2D>().gameObject;
+			//AudioSource.PlayClipAtPoint(pickupCoinClips, transform.position);
+			inkBottle.SendMessage ("InkBottleReact");
+		}
+		
+		if(coll.GetComponent<Collider2D>().CompareTag("Gum")) {
+			GameObject gum = coll.GetComponent<Collider2D>().gameObject;
+			//AudioSource.PlayClipAtPoint(pickupCoinClips, transform.position);
+			gum.SendMessage ("GumReact");
+		}
+		
+		if(coll.GetComponent<Collider2D>().CompareTag("Goal")) {
+			GameObject goal = coll.GetComponent<Collider2D>().gameObject;
+			//AudioSource.PlayClipAtPoint(pickupCoinClips, transform.position);
+			goal.SendMessage ("GoalReact");
+		}
+		
+		if(coll.GetComponent<Collider2D>().CompareTag("DownCollider")) {
+			print ("down collider");
+			AudioSource.PlayClipAtPoint(explosionClips, transform.position);
+			Time.timeScale = 0; // pauznutie hry
+			winPanel.SetActive(true);
+			GameObject btnInteractable = GameObject.Find("NextLvlBtn");
+			btnInteractable.GetComponent<Button>().interactable = false;
+			GameObject btnInteractableBack = GameObject.Find("BackToGameBtn");
+			btnInteractableBack.GetComponent<Button>().interactable = false;
+		}
+	}*/
 }
