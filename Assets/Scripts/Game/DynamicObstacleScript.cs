1a689e42dc6bd189b615103d3fd58dd11940602e
msukenik <m45sukenik@gmail.com>
2015-11-30 22:18:47 +0100

    ggg

--- Assets/Scripts/Game/DynamicObstacleScript.cs
+++ Assets/Scripts/Game/DynamicObstacleScript.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class DynamicObstacleScript : MonoBehaviour {
 	public bool horizontalMove = true;
 	public float elevatorSpeed = 1.5f; 
-	public float wallLeft = 0.0f;       // Define wallLeft
-	public float wallRight = 5.0f;      // Define wallRight
+	public float startX = 0.0f;       // Define wallLeft
+	public float startY = 0.0f;       // Define wallLeft
+	public float midlleX = 2.5f;      // Define wallRight
+	public float midlleY = 2.5f;       // Define wallLeft
+	public float endX = 5.0f;      // Define wallRight
+	public float endY = 5.0f;       // Define wallLeft
 	private float elevatorDirection = 1.0f;
 	private Vector2 elevatorAmount;
 	private float originalX;
@@ -23,17 +27,17 @@ public class DynamicObstacleScript : MonoBehaviour {
 	void Update () {
 		if (horizontalMove) {
 			elevatorAmount.x = elevatorDirection * elevatorSpeed * Time.deltaTime;
-			if (elevatorDirection > 0.0f && transform.position.x >= originalX + wallRight) {
+			if (elevatorDirection > 0.0f && transform.position.x >= originalX + startX) {
 				elevatorDirection = -1.0f;
-			} else if (elevatorDirection < 0.0f && transform.position.x <= originalX - wallLeft) {
+			} else if (elevatorDirection < 0.0f && transform.position.x <= originalX - endX) {
 				elevatorDirection = 1.0f;
 			}
 			transform.Translate (elevatorAmount);
 		} else {
 			elevatorAmount.y = elevatorDirection * elevatorSpeed * Time.deltaTime;
-			if (elevatorDirection > 0.0f && transform.position.y >= originalY + wallRight) {
+			if (elevatorDirection > 0.0f && transform.position.y >= originalY + startX) {
 				elevatorDirection = -1.0f;
-			} else if (elevatorDirection < 0.0f && transform.position.y <= originalY - wallLeft) {
+			} else if (elevatorDirection < 0.0f && transform.position.y <= originalY - endY) {
 				elevatorDirection = 1.0f;
 			}
 			transform.Translate (elevatorAmount);
