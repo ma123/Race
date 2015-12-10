560e6c65a8270477a7985865d578b2980d7fdcc0
msukenik <m45sukenik@gmail.com>
2015-12-10 20:29:09 +0100

    rapid kolesa

---
+++ Assets/Scripts/WorldSelector/ScrollRectSnapScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollRectSnapScript : MonoBehaviour {
+	public RectTransform panel;
+	public Button[] bttns;
+	public RectTransform center;
+
+	private float[] distance;
+	private bool dragging = false;
+	private int bttnDistance;
+	private int minBtnNum;
+	// Use this for initialization
+	void Start () {
+		int bttnLength = bttns.Length;
+		distance = new float[bttnLength];
+
+		bttnDistance = (int) Mathf.Abs (bttns[1].GetComponent<RectTransform>().anchoredPosition.x - bttns[0].GetComponent<RectTransform>().anchoredPosition.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
