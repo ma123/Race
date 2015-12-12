66dd8d5b448e39a3bf05d7fdf7a011ec2374b023
msukenik <m45sukenik@gmail.com>
2015-12-12 23:21:50 +0100

    fonty + main menu zobrazenie poctu coin v panele

---
+++ Assets/Scripts/MainMenu/ShowCoinPanelScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowCoinPanelScript : MonoBehaviour {
+	private static int money = 0;
+	private static Text moneyText;
+
+	// Use this for initialization
+	void Start () {
+		money = PlayerPrefs.GetInt("money",0);
+		moneyText = gameObject.GetComponent<Text>();
+		RefreshScoreText ();
+	}
+	
+	private static void RefreshScoreText() {
+		moneyText.text = money.ToString();
+	}
+}
